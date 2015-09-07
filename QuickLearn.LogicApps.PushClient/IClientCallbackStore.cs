d6b67010b8577f03bfff80248a02bb2b4def8d6c
nihaue <nickh@quicklearn.com>
2015-09-07 14:54:07 -0700

    Initial commit of core code

---
+++ QuickLearn.LogicApps.PushClient/IClientCallbackStore.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace QuickLearn.LogicApps
+{
+    public interface IClientCallbackStore<TConfiguration, TOutput>
+    {
+        Task<IEnumerable<Callback<TConfiguration, TOutput>>> ReadCallbacksAsync();
+    }
+}
---
+++ QuickLearn.LogicApps.PushTrigger/ICallbackStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace QuickLearn.LogicApps
+{
+    public interface ICallbackStore<T>
+    {
+        Task WriteCallbackAsync(string triggerId, Uri callbackUri, T triggerConfig);
+
+        Task DeleteCallbackAsync(string triggerId);
+    }
+}
